aea810834449e8d0c557f3414c8ecbe6b51f216b
unknown <oladiranhaleem@gmail.com>
2023-05-05 10:15:30 +0100

    first commit

---
+++ figma_project.js
@@ -0,0 +1,25 @@
+
+// function myFunction (){
+// 	let x = document.querySelector("#myNavBar")
+// 	if (x.className === navBar) {
+// 		x.className += "responsive"
+// 	}else{
+// 		x.className =navBar
+// 	}
+// }
+
+
+const input = document.querySelector("#Email")
+const boxes = document.querySelector (".boxes")
+const navbar = document.querySelector (".navbar")
+
+input.addEventListener("click", (e) => {
+	e.preventDefault ()
+
+	alert ("You are welcome");
+	
+
+})
+
+console.log(hello world)
+
